c43135f7c4afdaba82e39cf15805ba490fce98d6
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-08-01 23:54:57 +0530

    all set

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css/pagination";
 import "swiper/css";
+import Link from "next/link";
 
 const page = () => {
   return (
@@ -587,8 +588,72 @@ const page = () => {
       </div>
 
       {/* Our Blog */}
-      <div className="w-full h-[150vh]">
-        
+      <div className="w-full h-[123vh]">
+        <div className="mt-14">
+          <h3 className="font-['Fredoka'] text-[#DB6885] font-[900] text-3xl text-center">
+            Our Blog
+          </h3>
+          <h1 className="font-['figtree'] text-[8.7vh] text-center font-[900] mt-4">
+            Latest Post
+          </h1>
+        </div>
+
+        <div className="w-full h-[70vh] flex items-center justify-between px-4 mt-18">
+          <div className="left h-full w-[31vw] flex flex-col gap-7">
+            <Image
+              className="w-full h-[43vh] rounded-2xl object-cover"
+              src="/image/article-1.jpg"
+              alt="article-1"
+              width={1000}
+              height={1000}
+            />
+            <h1 className="text-2xl text-[#2F3137] font-['figtree'] font-[900] tracking-tight leading-[1.5] w-[30vw]">
+              How To Cook The Sweet Cupcakes with Strawberry Honey
+            </h1>
+            <Link
+              className="font-['Fredoka'] font-[900] text-[#DB6885] hover:underline"
+              href=""
+            >
+              Read More ››
+            </Link>
+          </div>
+          <div className="middle h-full w-[31vw] flex flex-col gap-7">
+            <Image
+              className="w-full h-[43vh] rounded-2xl object-cover"
+              src="/image/article-2.jpg"
+              alt="article-2"
+              width={1000}
+              height={1000}
+            />
+            <h1 className="text-2xl text-[#2F3137] font-['figtree'] font-[900] tracking-tight leading-[1.5] w-[30vw]">
+              Donut topping recommendations for donut lovers
+            </h1>
+            <Link
+              className="font-['Fredoka'] font-[900] text-[#DB6885] hover:underline"
+              href=""
+            >
+              Read More ››
+            </Link>
+          </div>
+          <div className="right h-full w-[31vw] flex flex-col gap-7">
+            <Image
+              className="w-full h-[43vh] rounded-2xl object-cover"
+              src="/image/article-3.jpg"
+              alt="article-3"
+              width={1000}
+              height={1000}
+            />
+            <h1 className="text-2xl text-[#2F3137] font-['figtree'] font-[900] tracking-tight leading-[1.5] w-[30vw]">
+              How To Cook The Sweet Macarons with Strawberry Cream
+            </h1>
+            <Link
+              className="font-['Fredoka'] font-[900] text-[#DB6885] hover:underline"
+              href=""
+            >
+              Read More ››
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,169 @@
+import { Clock5, Mail, Phone } from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="relative w-full h-[115.6vh]">
+      <div>
+        <Image
+          className="w-full h-[115.6vh]"
+          src="/image/footer.jpg"
+          alt="footer"
+          width={1000}
+          height={1000}
+        />
+
+        {/* newsletter-section */}
+        <div className="w-full absolute top-0 px-5 py-[11.95vh] flex items-center justify-between">
+          <h1 className="text-[4.05vw] text-white font-['figtree'] font-[900]">
+            Newsletter Subscribe
+          </h1>
+
+          <div className="flex items-center gap-5">
+            <input
+              type="text"
+              className="placeholder:text-white text-white border-white w-[66vh] border-[2.5px] outline-none px-[7vh] py-[3.1vh] rounded-full font-['figtree'] font-[900]"
+              placeholder="Email Address"
+            />
+            <button className="text-[#DB6885] bg-[#fff] font-['figtree'] px-[7.2vh] tracking-tight py-[3.2vh] rounded-full font-[900] text-[1.2vw]">
+              Subscribe Now
+            </button>
+          </div>
+        </div>
+
+        {/* newsletter underline */}
+        <div className="absolute top-[35vh] w-[96.5vw] border-[0.1px] border-[#ffffff61] left-1/2 -translate-x-1/2"></div>
+
+        {/* footer Links Section */}
+        <div className="absolute top-[45.9vh] w-full h-[53vh] px-5 flex items-center gap-8">
+          {/* logo */}
+          <div className="w-[67vh] h-full flex flex-col gap-5">
+            <Image
+              src="/image/logo-white.webp"
+              alt="logo-white"
+              width={130}
+              height={130}
+            />
+            <div className="email text-white font-['figtree'] flex items-center gap-2">
+              <Mail className="text-white font-[100] w-4 h-4" />
+              <h1 className="text-lg font-[900] tracking-tight">Email us :</h1>
+              <p>abhinavsharmaas20000@gmail.com</p>
+            </div>
+            <div className="email text-white font-['figtree'] flex items-center gap-2">
+              <Phone className="text-white font-[100] w-4 h-4" />
+              <h1 className="text-lg font-[900] tracking-tight">Call :</h1>
+              <p>+91-9084773936</p>
+            </div>
+            <div className="email text-white font-['figtree'] flex items-center gap-2">
+              <Clock5 className="text-white font-[100] w-4 h-4" />
+              <h1 className="text-lg font-[900] tracking-tight">
+                Working Hours
+              </h1>
+              <p>Monday - Friday, 08 am - 05 pm</p>
+            </div>
+            {/* social-links */}
+            <div className="text-white flex items-center gap-5">
+              <div className="w-9 h-9 flex items-center justify-center rounded-full bg-[#ffffff43] hover:bg-white hover:text-black  text-xl">
+                <FaFacebookF />
+              </div>
+              <div className="w-9 h-9 flex items-center justify-center rounded-full bg-[#ffffff43] hover:bg-white hover:text-black  text-2xl">
+                <IoLogoInstagram />
+              </div>
+              <div className="w-9 h-9 flex items-center justify-center rounded-full bg-[#ffffff43] hover:bg-white hover:text-black  text-2xl">
+                <FaWhatsapp />
+              </div>
+            </div>
+          </div>
+
+          {/* Quick Link 1 */}
+          <div className="w-[40vh] h-full text-white flex flex-col gap-6">
+            <h1 className="font-['Fredoka'] font-[900] text-xl mb-2">
+              Quick Links 1
+            </h1>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/about"
+            >
+              About
+            </Link>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/menu"
+            >
+              Menu
+            </Link>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/blog"
+            >
+              Blog
+            </Link>
+          </div>
+          {/* Quick Link 2 */}
+          <div className="w-[40vh] h-full text-white flex flex-col gap-6">
+            <h1 className="font-['Fredoka'] font-[900] text-xl mb-2">
+              Quick Links 2
+            </h1>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/"
+            >
+              My favorites
+            </Link>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/about"
+            >
+              My Cart
+            </Link>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/menu"
+            >
+              Empty State
+            </Link>
+            <Link
+              className="font-['figtree'] font-[600] hover:underline"
+              href="/blog"
+            >
+              Contact State
+            </Link>
+          </div>
+          {/* Download */}
+          <div className="w-[50vh] h-full font-['Fredoka'] text-white flex flex-col gap-6">
+            <h1 className="font-['Fredoka'] font-[900] text-xl mb-2">
+              Download Our App
+            </h1>
+            <Image
+              className="w-[12vw]"
+              src="/image/btn-google-play.webp"
+              alt="appStore"
+              width={300}
+              height={300}
+            />
+            <Image
+              className="w-[12vw]"
+              src="/image/btn-app-store.webp"
+              alt="googlePlay"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
